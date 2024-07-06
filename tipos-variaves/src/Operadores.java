571cb4620f096db7805af1611f078d0b8dd7530a
Natanael <natanael.martins.junior@live.com>
2024-07-05 22:14:09 -0300

    Operadores ternario e sua utilidade

--- tipos-variaves/src/Operadores.java
+++ tipos-variaves/src/Operadores.java
@@ -5,6 +5,30 @@ public class Operadores {
         String nomeCompleto = nomme + " " + sNome ;
         System.out.println(nomeCompleto);
 
+        int idade = - 30;
+        System.out.println(idade);
+        idade = -idade;
+        System.out.println(idade);
+
+     /* operadores ternários subistituindo o if / else */
+     
+     int a = 5;
+     int b = 2;
+    String resultado = ""; 
+     if (a==b){
+        resultado = "verdadeiro";
+     }
+     else
+        resultado = "falso";
+
+     System.out.println(resultado);
+
+     resultado = a==b? "verdadeiro" : "falso";
+
+     System.out.println(resultado);
+
+     
+
    
     }
 
