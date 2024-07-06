690406d6a58588d3cc55de28619d85402767915b
Natanael <natanael.martins.junior@live.com>
2024-07-05 21:41:43 -0300

    Operadores concatenção string

---
+++ tipos-variaves/src/Operadores.java
@@ -0,0 +1,13 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String nomme = "Natanael Martins";
+        String sNome = "Junior";
+        String nomeCompleto = nomme + " " + sNome ;
+        System.out.println(nomeCompleto);
+
+   
+    }
+
+    
+
+}
