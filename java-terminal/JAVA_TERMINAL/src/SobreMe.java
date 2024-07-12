f0b8755f17916d36be46181f68e7757ca25a1664
Natanael <natanael.martins.junior@live.com>
2024-07-11 22:31:01 -0300

    Utilizando Class Scan

--- java-terminal/JAVA_TERMINAL/src/SobreMe.java
+++ java-terminal/JAVA_TERMINAL/src/SobreMe.java
@@ -1,12 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class SobreMe {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         
-        String primeiroNome = args[0];
-        String segundoNome = args [1];
-        int idade = Integer.valueOf(args[2]);
-        double altura = Double.valueOf(args[3]);
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Digite seu Primeiro nome:");
+        String primeiroNome = scanner.next();
 
+        System.out.println("Digite Seu segundo Nome:");
+        String segundoNome = scanner.next();
 
+        System.out.println("Digite sua idade:");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua Altura:");
+        double altura = scanner.nextDouble();
+
+        System.out.println("*******************");
         System.out.println("Olá, mechamo "+ primeiroNome + " "+ segundoNome);
         System.out.println("Tenho:"+ idade);
         System.out.println("medindo:"+ altura);
