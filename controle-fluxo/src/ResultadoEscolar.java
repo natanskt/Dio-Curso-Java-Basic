b129c29d7e52e8583257b2ac9d02b87b4a598dc3
Natanael <natanael.martins.junior@live.com>
2024-07-15 22:01:06 -0300

    Substituindo if else por condicional ternaria

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,13 +1,22 @@
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 7;
+        int nota = 8;
 
-        if (nota <= 6) {
+     /*    if (nota == 7) {
             System.out.println("Você foi reprovado");
             
         }
+        else if (nota >= 5 && nota < 7) {
+            System.out.println("Você está em recuperação");
+            
+        }
+        
         else System.out.println("Prabéns você foi aprovado");
+*/
 
-    
+// Subistituindo todo código acima por condicional ternaria
+
+        String resultado = nota >=7  ? "APROVADO" : nota >= 5 && nota <7 ? "Você está em recuperação" : "reprovado";
+        System.out.println(resultado);
     }
 }
