d35fec93a6a47caa9d25aa54821152d9b954a551
Natanael <natanael.martins.junior@live.com>
2024-07-16 19:48:22 -0300

    Implementando instrução for

---
+++ controle-fluxo/src/ExemploFor.java
@@ -0,0 +1,9 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+
+        for (int carneiros = 0 ; carneiros <20 ; carneiros ++  ){
+            System.out.println("Numero de carneiros"+ carneiros);
+        }
+        
+    }
+}
