162fb47695f45c4d24464af34b5d7bc084fe325f
Natanael <natanael.martins.junior@live.com>
2024-07-16 20:04:58 -0300

    Implementando o foreach

--- controle-fluxo/src/ExemploFor.java
+++ controle-fluxo/src/ExemploFor.java
@@ -1,15 +1,22 @@
 public class ExemploFor {
     public static void main(String[] args) {
 
-        for (int carneiros = 0 ; carneiros <20 ; carneiros ++  ){
+      /* for (int carneiros = 0 ; carneiros <20 ; carneiros ++  ){
             System.out.println("Numero de carneiros"+ carneiros);
-        }
+        }   */
 
         String alunos [] = {"Maria", "Natanael", "Marcos", "Paula"};
 
-        for(int x = 0 ; x < alunos.length ; x++){
+      /*  for(int x = 0 ; x < alunos.length ; x++){
             System.out.println("Aluno N°:" + x + " " + alunos[x]);
+        }    */
+
+        for (String aluno : alunos) {
+            System.out.println("Nome do aluno : "+aluno);
+            
         }
+
+
 
     }
 }
