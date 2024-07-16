fe5a99784f9c2c65460f6e162ea77aba2f986631
Natanael <natanael.martins.junior@live.com>
2024-07-16 19:55:22 -0300

    Implementado for com pequena lista

--- controle-fluxo/src/ExemploFor.java
+++ controle-fluxo/src/ExemploFor.java
@@ -4,6 +4,12 @@ public class ExemploFor {
         for (int carneiros = 0 ; carneiros <20 ; carneiros ++  ){
             System.out.println("Numero de carneiros"+ carneiros);
         }
-        
+
+        String alunos [] = {"Maria", "Natanael", "Marcos", "Paula"};
+
+        for(int x = 0 ; x < alunos.length ; x++){
+            System.out.println("Aluno N°:" + x + " " + alunos[x]);
+        }
+
     }
 }
