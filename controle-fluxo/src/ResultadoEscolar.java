cef39d33aba7fab3e0d57a2d44a6321e0a818276
Natanael <natanael.martins.junior@live.com>
2024-07-15 21:26:40 -0300

    fluxos dados

---
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,13 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        int nota = 7;
+
+        if (nota <= 6) {
+            System.out.println("Você foi reprovado");
+            
+        }
+        else System.out.println("Prabéns você foi aprovado");
+
+    
+    }
+}
