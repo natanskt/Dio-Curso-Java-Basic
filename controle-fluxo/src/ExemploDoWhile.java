df8baedb7b539a06fdd0a94f863e039d07c03437
Natanael <natanael.martins.junior@live.com>
2024-07-17 21:34:53 -0300

    Exemplo do While -

---
+++ controle-fluxo/src/ExemploDoWhile.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+public class ExemploDoWhile {
+    public static void main(String[] args) {
+        System.out.println("Discando");
+
+        do  {
+            System.out.println("Telefone Tocando!!");
+        }
+
+        while (tocando());
+            System.out.println("Alô");
+            
+        
+        
+    }
+
+    private static boolean tocando() {
+        boolean atendeu = new Random().nextInt(3)==1;
+        System.out.println("atendeu"+ atendeu);
+        return ! atendeu;
+
+
+    }
+
+    
+}
